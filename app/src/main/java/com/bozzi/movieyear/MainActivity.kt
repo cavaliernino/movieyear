791492db25e8412424050285503bbccec13a35d8
Nino Bozzi <n.bozzi@mbmlatam.cl>
2019-08-22 10:27:22 -0400

    changes to nav drawer, to adapt only Watch list button, without action yet. TODO: add action to watch list button and add TMDB references.

--- app/src/main/java/com/bozzi/movieyear/MainActivity.kt
+++ app/src/main/java/com/bozzi/movieyear/MainActivity.kt
@@ -28,11 +28,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         component.inject(this)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
@@ -69,22 +64,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
+            R.id.nav_watchlist -> {
 
             }
         }
